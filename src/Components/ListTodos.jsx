7447e6811b47d312f13b2ae979defb1f3f9bdb3c
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-07-14 10:59:00 +0200

    implented list of todo change

--- src/Components/ListTodos.jsx
+++ src/Components/ListTodos.jsx
@@ -1,21 +1,48 @@
+import React, { useState } from 'react';
+
 export default function ListTodos() {
-    return (
-        <section>
-        <h2>Todos</h2>
-        <ul>
-          <li>
-          <input type="checkbox" name="" id="" />
-          être reconnaisante
+  let initTodos = [
+        {
+            id: '1',
+            name: 'être reconnaisante',
+            done: 'true'
+        },
+        {
+            id: '2',
+            name: 'Apprendre React',
+            done: 'false'
+        },
+        {
+            id: '3',
+            name: 'Rager sur LOL',
+            done: 'true'
+        }
+  ];
+
+  const [todos, setTodos] = useState(initTodos);
+
+  const handleCheckboxClick = (e) => {
+    const newTodos = todos.map((todo) => {
+        if (todo.id === e.target.id) {
+            todo.done = !todo.done
+        }
+        return todo
+    })
+    setTodos(newTodos)
+  }
+
+  return (
+    <section>
+      <h2>Todos</h2>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            <input type="checkbox" id='todo.id' checked={todo.done} onClick={handleCheckboxClick}/>
+            {todo.name}
           </li>
-          <li>
-          <input type="checkbox" name="" id="" />
-            <span>Apprendre React</span>
-          </li>
-          <li>
-          <input type="checkbox" name="" id="" />
-          Rager sur LOL
-          </li>
-        </ul>
-        </section>
-    )
+        ))}
+      </ul>
+      <button >Add a todo</button>
+    </section>
+  );
 }
