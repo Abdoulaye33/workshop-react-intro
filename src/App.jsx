b9a2b786574fe77517200efcb4ab5482e77060a1
Abdoulaye33 <Abdulaibayo08@outlook.fr>
2023-07-14 10:08:52 +0200

    Create basic app structure with

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import '@picocss/pico'
+import Header from './Components/header'
+import AddTodo from './Components/AddTodo'
+import ListTodos from './Components/ListTodos'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+
       <main className="container">
-        Hello Worlds !
+        
+
+      <Header />
+      <AddTodo />
+      <ListTodos />
+
+
+
       </main>
     </>
   )
